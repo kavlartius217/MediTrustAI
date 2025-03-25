45eca065e63e55501aaf3f92088c204a49d4b831
Kavin Lartius <kavlartius@gmail.com>
2025-03-25 18:01:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from crewai.flow import Flow, start, listen, and_, or_, router
+from langchain_community.document_loaders import PyPDFLoader
 from pydantic import BaseModel
 from major_crew import Medic_Bot
 from doctor_crew import Doctor_Bot
@@ -8,13 +9,19 @@ nest_asyncio.apply()
 class State(BaseModel):
   abnormalities:str=" "
   decision:str=" "
+  report:str=" 
 
 
 class MediTrustAI(Flow[State]):
+
   @start()
   def medic_bot(self):
+    report=PyPDFLoader("/content/WM17S.pdf")
+    report=report.load()
+    report= "\n\n".join([doc.page_content for doc in report])
+    self.state.report=report
     result=(
-        Medic_Bot().crew().kickoff({"report":report})
+        Medic_Bot().crew().kickoff({"report":self.state.report})
     )
     self.state.abnormalities=result.raw
 
@@ -33,6 +40,7 @@ class MediTrustAI(Flow[State]):
 
   @listen("Proceed")
   def doctor_bot(self):
+    user_city=input("Enter your city")
     result=(
-        Doctor_Bot().crew().kickoff({"user_city":"Pune","abnormalities":self.state.abnormalities})
+        Doctor_Bot().crew().kickoff({"user_city":user_city,"abnormalities":self.state.abnormalities})
     )
